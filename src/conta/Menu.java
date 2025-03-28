8b4cdaae21ad6f50d9d89500e1fb8bfafe5eb642
ÃIago Wilian <agowillian22@gmail.com>
2025-03-28 10:25:21 -0300

    atualização da estrutura

---
+++ src/conta/Menu.java
@@ -0,0 +1,116 @@
+package conta;
+
+import java.util.Scanner;
+
+<<<<<<< HEAD
+=======
+import conta.model.Conta;
+>>>>>>> 6e81f76 (ajustando estrutura de pasta)
+import conta.util.Cores;
+
+public class Menu {
+
+	public static void main(String[] args) {
+<<<<<<< HEAD
+		// TODO Auto-generated method stub
+=======
+		
+		// Teste da Classe Conta
+		Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
+		c1.vizualizar();
+		c1.sacar(12000.0f);
+		c1.vizualizar();
+		c1.depositar(5000.0f);
+		c1.vizualizar();
+		
+>>>>>>> 6e81f76 (ajustando estrutura de pasta)
+		Scanner leia = new Scanner(System.in);
+
+		int opcao;
+
+		while (true) {
+
+			System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND
+					+ "*****************************************************");
+			System.out.println("                                                     ");
+			System.out.println("                BANCO DO BRAZIL COM Z                ");
+			System.out.println("                                                     ");
+			System.out.println("*****************************************************");
+			System.out.println("                                                     ");
+			System.out.println("            1 - Criar Conta                          ");
+			System.out.println("            2 - Listar todas as Contas               ");
+			System.out.println("            3 - Buscar Conta por Numero              ");
+			System.out.println("            4 - Atualizar Dados da Conta             ");
+			System.out.println("            5 - Apagar Conta                         ");
+			System.out.println("            6 - Sacar                                ");
+			System.out.println("            7 - Depositar                            ");
+			System.out.println("            8 - Transferir valores entre Contas      ");
+			System.out.println("            9 - Sair                                 ");
+			System.out.println("                                                     ");
+			System.out.println("*****************************************************");
+			System.out.println("Entre com a opção desejada:                          ");
+			System.out.println("                                                     " + Cores.TEXT_RESET);
+
+			opcao = leia.nextInt();
+			if (opcao == 9) {
+				System.out.println(Cores.TEXT_WHITE_BOLD + "\nBanco do Brazil com Z - o seu futuro começa aqui!");
+				sobre();
+				leia.close();
+				System.exit(0);
+			}
+
+			switch (opcao) {
+			case 1:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Criar conta \n\n");
+
+				break;
+			case 2:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Listar todas as contas\n\n");
+
+				break;
+			case 3:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Consultar dados da conta - por número\n\n");
+
+				break;
+			case 4:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Atualizar dados da conta\n\n");
+
+				break;
+			case 5:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Apagar a conta\n\n");
+
+				break;
+			case 6:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Saque\n\n");
+
+				break;
+			case 7:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Depósito\n\n");
+
+				break;
+			case 8:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Transferência entre contas\n\n");
+
+				break;
+			default:
+				System.out.println(Cores.TEXT_RED_BOLD + "Opção Inválida!\n" + Cores.TEXT_RESET);
+				break;
+			}
+		}
+	}
+
+	public static void sobre() {
+		System.out.println("\n*********************************************************");
+		System.out.println("Projeto Desenvolvido por: ");
+		System.out.println("Iago Willian - iagowillian22@gmail.com");
+		System.out.println("github.com/IagoWiliian");
+		System.out.println("*********************************************************");
+	}
+}
+<<<<<<< HEAD
+
+	
+
+
+=======
+>>>>>>> 6e81f76 (ajustando estrutura de pasta)
